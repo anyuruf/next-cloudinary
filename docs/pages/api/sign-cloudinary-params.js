c2fd22749f97bc5914ab400b5c44cef598591f9c
Anyuruf <46653783+anyuruf@users.noreply.github.com>
2023-09-03 12:02:51 +0300

    Update sign-cloudinary-params.js
    I think Error on error as e.message is undefined

--- docs/pages/api/sign-cloudinary-params.js
+++ docs/pages/api/sign-cloudinary-params.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     res.status(200).json({
       signature,
     });
-  } catch (error) {
+  } catch (e) {
     res.status(500).json({
       error: e.message,
     });
